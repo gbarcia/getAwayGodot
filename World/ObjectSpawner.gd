8988c4df5e82242c5adcd0652e9be9be99f54b3d
Gerardo Barcia <Personal@192.168.68.111>
2021-02-06 23:48:21 +0100

    adding dumpsters

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -8,6 +8,7 @@ var numnber_of_billboards = 75
 var number_of_traffic_cones = 40
 var number_of_hydrants = 50
 var number_of_streetlights = 50
+var number_of_ramps = 25
 
 func generate_props(tile_list,size):
 	tiles = tile_list
@@ -28,7 +29,7 @@ func random_tile(tile_count):
 	return selected_tiles
 	
 func place_cars():
-	var tiles_list = random_tile(number_of_parked_cars)
+	var tiles_list = random_tile(number_of_parked_cars + number_of_ramps)
 	for i in range(number_of_parked_cars):
 		var tile = tiles_list[0]
 		var tile_type = get_node("..").get_cell_item(tile.x,0,tile.z)
@@ -38,12 +39,31 @@ func place_cars():
 			tile.y = tile.y + 0.5
 			rpc("spawn_cars",tile,tile_rotation)
 		tiles_list.pop_front()
+	place_ramps(tiles_list)
 			
 sync func spawn_cars(tile,car_rotation):
 	var car = preload("res://Props/ParkedCars.tscn").instance()
 	car.translation = Vector3((tile.x * 20 ) + 10, tile.y,(tile.z * 20) + 10)
 	car.rotation_degrees.y = car_rotation
 	add_child(car)
+	
+	
+func place_ramps(tiles_list):
+	for i in range(number_of_ramps):
+		var tile = tiles_list[0]
+		var tile_type = get_node("..").get_cell_item(tile.x,0,tile.z)
+		var allowed_rotations = $ObjectRotLookup.lookup_rotation(tile_type)
+		if not allowed_rotations == null:
+			var tile_rotation = allowed_rotations[randi() % allowed_rotations.size() - 1] * -1
+			tile.y = tile.y + 0.5
+			rpc("spawn_ramps",tile,tile_rotation)
+		tiles_list.pop_front() 
+			
+sync func spawn_ramps(tile,ramp_rotation):
+	var ramp = preload("res://Props/Dumpster/Dumpster.tscn").instance()
+	ramp.translation = Vector3((tile.x * 20 ) + 10, tile.y,(tile.z * 20) + 10)
+	ramp.rotation_degrees.y = ramp_rotation
+	add_child(ramp)
 	
 func place_billboards():
 	var tiles_list = random_tile(numnber_of_billboards)
